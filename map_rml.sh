dee310e2026cabae2942b8485dc2beff9aa6c38e
Pavel Zhelnov <pzhelnov@p1m.org>
2024-06-21 03:14:02 -0400

    Tweak mapping script to add prefix to mapped ontology
    So that the name is not identical to the ttl from schema dir.

--- map_rml.sh
+++ map_rml.sh
@@ -5,6 +5,6 @@ ttl_dir="gbad/mapping/target"
 
 rml=$(find "$rml_dir" -type f -name "*.rml")
 rml_filename=$(basename "$rml" .rml)
-ttl="$ttl_dir/$rml_filename.ttl"
+ttl="mapped_$ttl_dir/$rml_filename.ttl"
 
 java -jar rmlmapper* -s turtle -m "$rml" -o "$ttl"
