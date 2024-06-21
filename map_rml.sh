c66ee3ccdc5a49f456e0616ca7439fecd0d9ac58
Pavel Zhelnov <pzhelnov@p1m.org>
2024-06-20 23:07:29 -0400

    Create shell script for easy rml mapping

---
+++ map_rml.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rml_dir="gbad/schema/authority"
+ttl_dir="gbad/mapping/target"
+
+rml=$(find "$rml_dir" -type f -name "*.rml")
+rml_filename=$(basename "$rml" .rml)
+ttl="$ttl_dir/$rml_filename.ttl"
+
+java -jar rmlmapper* -s turtle -m "$rml" -o "$ttl"
